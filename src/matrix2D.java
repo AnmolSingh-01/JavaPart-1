50f349fece20da95eae5ca12da5dcc26dd5a68a4
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-11-23 01:26:43 +0530

    Update matrix2D.java

--- src/matrix2D.java
+++ src/matrix2D.java
@@ -1,25 +1,28 @@
-import java.util.*;
-
-class Array { 
-  
-    public static void convertToString(int arr[][]) 
-    { 
-
-        for (int n = 0 ; n < arr.length ; n++)
-        {
-            System.out.println(Arrays.toString(arr[n])); 
-        } 
-            
-    } 
-  
-    public static void main(String args[])
-    { 
-        int ar[][] = { { 10, 20, 30 }, 
-                        { 40, 50, 60 }, 
-                        { 70, 80, 90 } }; 
-        convertToString(arr); 
-    } 
-} 
+// Printing of matrix using JAVA 2D array --> Method-1 : using 2 for loops , complexity is O(n*n)
+import java.io.*;
+ 
+class Matrix {
+ 
+    public static void print2D(int arr[][])
+    {
+        // Loop through all rows
+        for (int i = 0; i < arr.length;i++) {
+ 
+            // Loop through all elements of current row
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j] + " ");
+                   System.out.println();          
+        }
+    }
+ 
+    public static void main(String args[]) throws IOException
+    {
+        int arr[][] = { { 10,20,30,40,50 },
+                        { 40,80,100,13,45},
+                        { 90,90,80,70,22} };
+        print2D(arr);
+    }
+}
 // Method 2 
 
 /*

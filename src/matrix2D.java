674becce875085921cd6a1505b3916ac785d7493
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-11-23 00:00:02 +0530

    Update matrix2D.java

--- src/matrix2D.java
+++ src/matrix2D.java
@@ -20,3 +20,39 @@ class Array {
         convertToString(arr); 
     } 
 } 
+// Method 2 
+
+/*
+class Array { 
+  
+    public static void convertToString(int arr[][]) 
+    { 
+        int n = 0,k = 0;
+        while (n != arr.length)
+        {
+            while (k != arr[n].length)
+            {
+                System.out.print(arr[n][k] + " ");
+                k++;
+            }
+            k = 0;
+            n++;
+            System.out.println("");
+        }
+            
+    } 
+  
+    public static void main(String args[])
+    { 
+        int arr[][] = { { 23, 70, 39 }, 
+                        { 51, 64, 47 }, 
+                        { 81, 11, 105 } }; 
+        convertToString(arr); 
+    } 
+}
+
+
+
+
+
+*/
